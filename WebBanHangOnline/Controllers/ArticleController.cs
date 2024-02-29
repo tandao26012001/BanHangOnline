5b51b57dbab1bc02f7e19fbbf1d2b2b25b9d46c6
tandao26012001 <44472411+tandao26012001@users.noreply.github.com>
2024-02-29 14:56:30 +0700

    init project

---
+++ WebBanHangOnline/Controllers/ArticleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private WebBanHangOnlineDbContext db = new WebBanHangOnlineDbContext();
+        // GET: Article
+        public ActionResult Index(string alias)
+        {
+            var item = db.Posts.FirstOrDefault(x => x.Alias == alias);
+            return View(item);
+        }
+    }
+}
---
+++ WebBanHangOnline/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebBanHangOnline.Models.WebBanHangOnlineDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebBanHangOnline.Models.WebBanHangOnlineDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
